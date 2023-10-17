25d47a55c93547603c064ebb5f4a0ef55fff5bf4
charansaikc@gmail.com <mailto:tpvakhilesh@gmail.com>
2023-10-17 16:16:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace Phase1Section4._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            string[] students = new string[10];
+            students[0] = "Kumar";
+            students[1] = "Mojesh";
+            students[2] = "Ajay";
+            students[3] = "Sai";
+            students[4] = "Naresh";
+            students[5] = "Fazal";
+            students[6] = "Tarun";
+            students[7] = "Ganesh";
+            students[8] = "Charandini";
+            students[9] = "Kedar";
+
+            bool flag = true;
+            string temp;
+            int numLength = students.Length;
+            for (int i = 1; (i <= (numLength - 1)) && flag; i++)
+            {
+                flag = false;
+                for (int j = 0; j < (numLength - 1); j++)
+                {
+                    if (students[j + 1].CompareTo(students[j]) < 0)
+                    {
+                        temp = students[j];
+                        students[j] = students[j + 1];
+                        students[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+
+
+            foreach (string s in students)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
+
